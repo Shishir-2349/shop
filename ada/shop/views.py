ab9ef013e26ddf34e23353aeb202e7841cb77013
Shishir-2349 <hasan.bup.24@gmail.com>
2024-12-28 16:40:32 +0600

    Second Commit

--- ada/shop/views.py
+++ ada/shop/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def show(response,*args,**kwargs):
-    return HttpResponse("hello_world. I fuck prostitutes nowadays")
+    return HttpResponse("hello_world. I fuck prostitutes nowadays. And it is good!")
